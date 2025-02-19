b33d3fdd18950c09ddd19f09183ae893ea30f576
dabamascodes <daniel.6@live.com.mx>
2025-02-18 20:32:48 -0700

    Custom Property Decorator - GetUser

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
-import { DataSource } from 'typeorm';
+import { GetUser } from './decorators/get-user.decorator';
+
+import { CreateUserDto, LoginUserDto } from './dto';
+import { User } from './entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+
 
   @Post('register')
   createUser(@Body() createUserDto: CreateUserDto) {
@@ -21,12 +25,22 @@ export class AuthController {
 
   @Get('private')
   @UseGuards( AuthGuard() )
-  testingPrivateRoute() {
+  testingPrivateRoute(
+    // @Req() request: Express.Request
+    @GetUser() user: User // TODO? -> Custom Decorator
+  ) {
+
+    // TODO! -> Fucnciona con: @Req() request: Express.Request
+    // console.log({ request });
+    // console.log({ user: request.user });
+
+    // TODO? -> Custom Decorator
+    console.log({ user })
 
     return {
       ok: true,
       message: 'Hola Mundo Private',
-      user: { name: 'name' }
+      user: user,
     }
   }
 
