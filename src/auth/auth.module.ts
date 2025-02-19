97e2882bd2f63625f21bf19f6ae2dfe495294545
dabamascodes <daniel.6@live.com.mx>
2025-02-18 18:06:11 -0700

    Se agrega el proyecto

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,43 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { User } from './entities/user.entity';
+
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService],
+  imports: [
+      TypeOrmModule.forFeature([ User ]),
+
+      PassportModule.register({ defaultStrategy: 'jwt' }),
+
+      JwtModule.registerAsync({
+        imports: [ ConfigModule ],
+        inject: [ ConfigService ],
+        useFactory: ( configService: ConfigService ) => {
+          // console.log('JWT Secret', configService.get('JWT_SECRET') )
+          // console.log('JWT SECRET', process.env.JWT_SECRET)
+          return {
+            // secret: process.env.JWT_SECRET,
+            secret: configService.get('JWT_SECRET'),
+            signOptions: {
+              expiresIn:'2h'
+            }
+          }
+        }
+      })
+      // JwtModule.register({
+      //   secret: process.env.JWT_SECRET,
+      //   signOptions: {
+      //     expiresIn:'2h'
+      //   }
+      // })
+  ],
+  exports: [ TypeOrmModule ]
+})
+export class AuthModule {}
