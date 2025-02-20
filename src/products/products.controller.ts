20d350cb9f2642a6c5d51345714412a848cb05ab
dabamascodes <daniel.6@live.com.mx>
2025-02-19 20:02:11 -0700

    Auth en otros módulos

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -4,11 +4,16 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
 
+import { Auth } from 'src/auth/decorators';
+import { ValidRoles } from 'src/auth/interfaces';
+
 @Controller('products')
+// @Auth()
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @Auth( ValidRoles.admin )
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
@@ -25,14 +30,16 @@ export class ProductsController {
   }
 
   @Patch(':id')
-    update(
-      @Param('id', ParseUUIDPipe) id: string, 
-      @Body() updateProductDto: UpdateProductDto
+  @Auth( ValidRoles.admin )
+  update(
+    @Param('id', ParseUUIDPipe) id: string, 
+    @Body() updateProductDto: UpdateProductDto
   ) {
     return this.productsService.update( id, updateProductDto );
   }
 
   @Delete(':id')
+  @Auth( ValidRoles.admin )
   remove(@Param( 'id', ParseUUIDPipe ) id: string) {
     return this.productsService.remove(id);
   }
